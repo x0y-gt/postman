d8f6ec964e647e8e9b3afc0bda50f27d41b1ab16
x0y <x0y@milkyweb.co>
2019-11-13 10:09:18 +0100

    [add] first commit

---
+++ postman/transports/smtp.py
@@ -0,0 +1,32 @@
+import yagmail
+from postman.transport import Transport
+from postman.message import Message
+
+__ALL__ = ['SmtpTransport']
+
+class SmtpTransport(Transport):
+    host = None
+
+    port = 25
+
+    def __init__(self, user=None, password=None, host=None, port=587):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.connect()
+
+    def connect(self):
+        self.smtp = yagmail.SMTP(
+            user=self.user,
+            password=self.password,
+            host=self.host
+        )
+
+    def send(self, msg: Message):
+        self.smtp.send(
+            to=",".join(msg._to),
+            subject=msg._subject,
+            contents = [msg.render()]
+        )
+
